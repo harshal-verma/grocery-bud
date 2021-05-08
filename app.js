cf8a9763e4f7ea573f5909d50e95dc808603ebef
harshal-verma <harshalvermaaaaa@gmail.com>
2021-05-08 20:38:30 +0530

    updated index.html and styles.css

---
+++ app.js
@@ -0,0 +1,31 @@
+let item = [
+    {
+        done: false,
+        text: 'go to gym',
+        id: 1,
+    },
+]
+const grocerryInputEl = document.getElementById('grocery-input');
+const groceryListEl = document.getElementById('grocery-list');
+
+function addItem(){
+
+}
+
+function deleteItem(){
+
+}
+
+function checkItem(){
+
+}
+
+function renderList(){
+
+}
+
+function initialize(){
+
+}
+
+initialize();
