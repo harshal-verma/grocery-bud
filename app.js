070151ce7976f8bf3017c89e7657b6faa17c6393
harshal-verma <harshalvermaaaaa@gmail.com>
2021-05-10 15:24:09 +0530

    updated app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const groceryListEl = document.getElementById('grocery-list');
 
 function addItem(item){
     items.push(item);
+    renderList();
 }
 
 function deleteItem(itemId){
@@ -17,19 +18,57 @@ function deleteItem(itemId){
         return item.id !== itemId
     });
     items = newItems;
+    renderList();
 }
 
-function checkItem(){
+function checkItem(itemId){
+    const checkIndex = items.findIndex(function(item){
+        return itemId === item.id;
+    })
 
+    items[checkIndex].done = !items[checkIndex].done
 }
 
 function renderList(){
+    groceryListEl.innerHTML = '';
+    for(let i = 0 ; i < items.length ; i++){
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <label for="${items[i].id}" >
+            <input id="${items[i].id}" type="checkbox"/>
+                ${items[i].text} 
+        <button class="delete" data-deleteId = "${items[i].id}">
+            delete 
+        </button>
+        `;
+        groceryListEl.appendChild(li);
+    }
+}
+
+function handleClick(event){
+    if(event.target.className === 'delete'){
+        const itemToBeDeleted = Number(event.target.dataset.deleteid);
+        deleteItem(itemToBeDeleted)
+    }
 
 }
 
 function initialize(){
+    document.addEventListener('click', handleClick);
+    document.addEventListener('keyup', function(e){
+            const text = groceryInputEl.value;
+            if(e.key === 'Enter'){
+            const item = {
+                done: false,
+                text: text,
+                id: Date.now(),
+            }
+            addItem(item)
+        }
+    })
     renderList();
 }
+
 
 initialize();
 
